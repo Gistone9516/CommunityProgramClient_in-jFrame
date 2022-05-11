fb15364248df8e32e2f989ae53321b3d33922d20
Gistone9516 <65705919+Gistone9516@users.noreply.github.com>
2022-05-11 16:54:09 +0900

    Create commentTable.java

---
+++ commentTable.java
@@ -0,0 +1,14 @@
+package CClient;
+
+public class commentTable {
+	String comment;
+	String commentNumber;
+	String userNumber;
+	String boardNumber;
+	commentTable(String _c, String _cn, String _un, String _bn){
+		this.comment = _c;
+		this.commentNumber = _cn;
+		this.userNumber = _un;
+		this.boardNumber = _bn;
+	}
+}
